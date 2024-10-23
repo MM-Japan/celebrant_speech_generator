adc0d27c7af65b0b0609ddfb16f4090ad488065e
Max McCain <itsmaximuk@gmail.com>
2024-10-23 17:44:57 +0900

    added follow-up questions

--- app/controllers/speech_requests_controller.rb
+++ app/controllers/speech_requests_controller.rb
@@ -3,17 +3,58 @@ class SpeechRequestsController < ApplicationController
     @speech_request = SpeechRequest.new
   end
 
+
   def create
     @speech_request = SpeechRequest.new(speech_request_params)
 
     if @speech_request.save
-      @generated_speech = ChatgptService.call(@speech_request)
-      @speech_request.update(generated_speech: @generated_speech) # Save the speech to the request if desired
-      redirect_to @speech_request, notice: 'The celebrant speech has been successfully generated.'
+      follow_up_questions = ChatgptService.generate_questions(@speech_request)
+
+      # Save the follow-up questions directly as a string
+      @speech_request.update(follow_up_questions: follow_up_questions)
+
+      # Redirect to the edit form where the follow-up questions will be answered
+      redirect_to edit_speech_request_path(@speech_request), notice: 'Initial information received. Please answer the following questions for more details.'
     else
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @speech_request = SpeechRequest.find(params[:id])
+
+    # Split the follow-up questions string into an array using newlines as the delimiter
+    @follow_up_questions = @speech_request.follow_up_questions.split("\n")
+  end
+
+
+
+  def update
+    @speech_request = SpeechRequest.find(params[:id])
+
+    # Debugging: print the params to see if detailed_answers is being submitted
+    puts params.inspect
+
+    # Safely collect the detailed answers from the form
+    if params[:speech_request][:detailed_answers].present?
+      detailed_answers = params[:speech_request][:detailed_answers].values.join("\n")
+    else
+      detailed_answers = "No additional details were provided."
+    end
+
+    if @speech_request.update(detailed_answers: detailed_answers)
+      final_speech = ChatgptService.generate_speech(@speech_request)
+      @speech_request.update(generated_speech: final_speech)
+
+      redirect_to @speech_request, notice: 'The celebrant speech has been successfully generated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+
+
 
   def show
     @speech_request = SpeechRequest.find(params[:id])
@@ -21,7 +62,17 @@ class SpeechRequestsController < ApplicationController
 
   private
 
+  # def speech_request_params
+  #   params.require(:speech_request).permit(
+  #     :name, :age, :relation, :childhood_overview, :work_overview,
+  #     :family_overview, :hobbies_overview, :travel_overview,
+  #     detailed_answers: {}
+  #   )
+  # end
+
   def speech_request_params
-    params.require(:speech_request).permit(:name, :relation, :memories)
+    params.require(:speech_request).permit!
   end
+
+
 end
